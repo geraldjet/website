f30d70df96436a1c5835b748133c68e6ac73f74e
geraldjet <geraldpromchuakaew@gmail.com>
2023-07-19 23:26:11 -0700

    web

---
+++ src/resumeData.js
@@ -0,0 +1,87 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Gerald Promchuakaew",
+    "role": "Web Developer",
+    "linkedinId":"268008255",
+    // "skypeid": "Your skypeid",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/gerald-promchuakaew-268008255/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/geraldjet",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am a highly motivated individual with a strong passion for software development and full stack development. As a recent computer science graduate, I am actively seeking opportunities to grow and advance in the field. I am eager to contribute my skills, collaborate with a dynamic team, and continuously expand my knowledge in order to tackle challenging projects and drive innovation in the software development industry.",
+    "address":"Burnaby, BC, Canada",
+    "website":"www.geraldjet.com",
+    "education":[
+      {
+        "UniversityName":"The University of British Columbia",
+        "specialization":"BSc Major in Computer Science",
+        "dateEnroll":"Sep 2018",
+        "dateGraduate":"May 2023",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Project: Human City",
+        "specialization":"Front End Developer",
+        "dateStart":"Jan 2023",
+        "dateLeave":"July 2023",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Typescript"
+      }
+    ],
+    "portfolio":[
+      
+      {
+        "name":"ApostleAI",
+        "description":"Conversation Bot modeled after Paul the Apostle",  
+        "imgurl":"images/portfolio/apostleai.jpg"
+      },
+      {
+        "name":"GameX",
+        "description":"Simple Blog Webiste created using html, css, php",
+        "imgurl":"images/portfolio/gamex.jpg"
+      },
+      {
+        "name":"tbd",
+        "description":"in progress",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"tbd",
+        "description":"in progress",
+        "imgurl":"images/portfolio/origami.jpg"
+      },
+    ],
+  
+  }
+  
+  export default resumeData
